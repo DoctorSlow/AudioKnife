35ba444efb51d4376994b651b8249511af2d3c3b
Jeremy J. Lee <doctorslow@gmail.com>
2018-06-09 20:54:06 -0700

    scraping articles, but not displaying on website yet

--- server.js
+++ server.js
@@ -46,22 +46,27 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with request
-  axios.get("https://pitchfork.com/").then(function (response) {
+  axios.get("https://www.tinymixtapes.com").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article-details").each(function (i, element) {
+    // Now, we grab every h4 within an article tag, and do the following:
+    $("article").each(function (i, element) {
       // Save an empty result object
       var result = {};
-
+      console.log(result)
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $('article').each
+
+      // result.title = $(this).find("h4.title_title").children("a").text();
+      result.link = $(this).find("a").attr("href");
+
+      // result.title = $(this)
+      //   .children("a")
+      //   .text();
+      // result.link = $(this)
+      //   .children("a")
+      //   .attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
